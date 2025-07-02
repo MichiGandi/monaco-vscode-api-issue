0323ecbc6eb6d52eb0f9df989f09d1c4aecf3c80
MichiGandi <Michi.Gandi@online.de>
2025-07-02 15:50:09 +0200

    improve adding workers
    "In the monaco-vscode-api repo's issues and demo project you will meet the following variant of adding workers:"

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@
 import './style.css'
 import * as monaco from 'monaco-editor';
 
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-
+export type WorkerLoader = () => Worker;
+const workerLoaders: Partial<Record<string, WorkerLoader>> = {
+	TextEditorWorker: () => new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker.js', import.meta.url), { type: 'module' })
+}
 window.MonacoEnvironment = {
-  getWorker: function (_moduleId, _label) {
-	return new editorWorker();
+  getWorker: function (_workerId, label) {
+	const workerFactory = workerLoaders[label]
+    if (workerFactory != null) {
+      return workerFactory()
+    }
+	throw new Error(`Worker ${label} not found`)
   }
 }
+
 monaco.editor.create(document.getElementById('editor')!, {
 	value: "Hello world!",
 });
