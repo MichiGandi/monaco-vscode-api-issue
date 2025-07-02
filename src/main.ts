df3460eda2b8fdf0e88be418dbc239dc999afd02
MichiGandi <Michi.Gandi@online.de>
2025-07-02 16:04:35 +0200

    import code

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@
 import './style.css'
 import * as monaco from 'monaco-editor';
 
+// importing installed services
+import { initialize } from 'vscode/services'
+import getLanguagesServiceOverride from "@codingame/monaco-vscode-languages-service-override";
+import getThemeServiceOverride from "@codingame/monaco-vscode-theme-service-override";
+import getTextMateServiceOverride from "@codingame/monaco-vscode-textmate-service-override";
+
+// adding worker
 export type WorkerLoader = () => Worker;
 const workerLoaders: Partial<Record<string, WorkerLoader>> = {
 	TextEditorWorker: () => new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker.js', import.meta.url), { type: 'module' })
@@ -16,6 +23,14 @@ window.MonacoEnvironment = {
   }
 }
 
+// adding services
+await initialize({
+    ...getTextMateServiceOverride(),
+    ...getThemeServiceOverride(),
+    ...getLanguagesServiceOverride(),
+});
+
 monaco.editor.create(document.getElementById('editor')!, {
-	value: "Hello world!",
+	value: "print('Hello world!')",
+	language: "python"
 });
