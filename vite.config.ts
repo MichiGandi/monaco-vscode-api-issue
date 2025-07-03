66bd6c66d33de76d2cfa58f69851606c266bde23
MichiGandi <Michi.Gandi@online.de>
2025-07-03 14:31:22 +0200

    fix textmate error

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import type { UserConfig } from 'vite'
 import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
 
 export default {
-   optimizeDeps: {
-       esbuildOptions: {
-         plugins: [importMetaUrlPlugin]
-       }
-     }
+  optimizeDeps: {
+    include: [
+      'vscode-textmate'
+    ],
+      esbuildOptions: {
+        plugins: [importMetaUrlPlugin]
+      }
+    }
 } satisfies UserConfig
